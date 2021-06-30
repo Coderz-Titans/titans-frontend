f7a2185900be19b991cc0e0c83a0fdb78e2a2394
latifaghassan <lalooshamayra@hotmail.com>
2021-06-30 10:35:25 +0300

    pull

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,14 +1,14 @@
-import Dropdown from 'react-bootstrap/Dropdown'
-import CategoryCreate from './CategoryCreate';
-import axios from 'axios';
+import Dropdown from "react-bootstrap/Dropdown";
+import CategoryCreate from "./CategoryCreate";
+import axios from "axios";
 // import { Button } from "bootstrap";
 import React, { Component } from "react";
 import Row from "react-bootstrap/Row";
-import DropdownButton from 'react-bootstrap/DropdownButton'
+import DropdownButton from "react-bootstrap/DropdownButton";
 
 export class category extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       show: true,
@@ -16,76 +16,81 @@ export class category extends Component {
       recipysData: [],
       sortedData: [],
       showSort: false,
-      userSearch:'breakfast',
-    }
+      userSearch: "breakfast",
+    };
   }
 
   searchChange = (e) => {
     e.preventDefault();
     this.setState({
       userSearch: e.target.value,
-    })
+    });
     console.log(this.state.userSearch);
-  }
-  
+  };
+
   //96fb6427bbb14ca69161ce3bd9b5a06c : ibrahim
   searchFunction = () => {
- axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=96fb6427bbb14ca69161ce3bd9b5a06c&query=${this.state.userSearch}&fillIngredients=true&number=24&addRecipeInformation=true`).then(response => {
-   console.log(response);
-   this.setState({
-    recipysData: response.data.results,
-   })
- }).catch(error => {
-   alert(error.message);
-  })
-}
-
-
-
+    axios
+      .get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey="96fb6427bbb14ca69161ce3bd9b5a06c"&query=${this.state.userSearch}&fillIngredients=true&number=24&addRecipeInformation=true`
+      )
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          recipysData: response.data.results,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
 
   functionl = async (value) => {
+    await axios
+      .get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey="96fb6427bbb14ca69161ce3bd9b5a06c&type"=${this.state.userSearch}&fillIngredients=true&number=25&addRecipeInformation=true&sort=${value}&sortDirection=desc`
+      )
+      .then((response) => {
+        console.log(response);
+        console.log(this.state.sortedData);
+        console.log(this.state.showSort);
+        this.setState({
+          sortedData: response.data.results,
+        });
 
-    await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=96fb6427bbb14ca69161ce3bd9b5a06c&type=${this.state.userSearch}&fillIngredients=true&number=25&addRecipeInformation=true&sort=${value}&sortDirection=desc`).then(response => {
-      console.log(response);
-      console.log(this.state.sortedData);
-      console.log(this.state.showSort);
-      this.setState({
-        sortedData: response.data.results,
+        console.log(this.state.showSort);
       })
-
-      console.log(this.state.showSort);
-    }).catch(
-      error => {
+      .catch((error) => {
         alert(error.message);
-      }
-    )
-    this.state.sortedData.splice(8, (this.state.sortedData.length))
+      });
+    this.state.sortedData.splice(8, this.state.sortedData.length);
     this.setState({
       showSort: true,
-    })
+    });
     console.log(this.state.sortedData);
-  }
+  };
 
   heartRate = () => {
     this.setState({
       heartNumber: this.state.heartNumber + 1,
-    })
-  }
+    });
+  };
 
   componentDidMount = async () => {
-    await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=96fb6427bbb14ca69161ce3bd9b5a06c&type=breakfast&fillIngredients=true&number=24&addRecipeInformation=true`).then(response => {
-      console.log(response);
-      this.setState({
-        recipysData: response.data.results,
+    await axios
+      .get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey="96fb6427bbb14ca69161ce3bd9b5a06c"&type=breakfast&fillIngredients=true&number=24&addRecipeInformation=true`
+      )
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          recipysData: response.data.results,
+        });
       })
-
-    }).catch(
-      error => {
+      .catch((error) => {
         alert(error.message);
-      }
-    )
-  }
-
+      });
+  };
 
   render() {
     return (
@@ -104,7 +109,11 @@ export class category extends Component {
                 aria-describedby="search-addon"
                 onChange={this.searchChange}
               />
-              <button type="button" class="btn btn-outline-primary" onClick={this.searchFunction}>
+              <button
+                type="button"
+                class="btn btn-outline-primary"
+                onClick={this.searchFunction}
+              >
                 search
               </button>
             </div>
@@ -112,34 +121,67 @@ export class category extends Component {
 
           <Row id="DropdownButton">
             <DropdownButton id="dropdown-basic-button" title="Favorite types">
-              <Dropdown.Item href="#/action-1" value="popularity" id="BreakfastId" onClick={() => this.functionl(document.getElementById('BreakfastId').getAttribute('value'))}> Popularity</Dropdown.Item>
+              <Dropdown.Item
+                href="#/action-1"
+                value="popularity"
+                id="BreakfastId"
+                onClick={() =>
+                  this.functionl(
+                    document.getElementById("BreakfastId").getAttribute("value")
+                  )
+                }
+              >
+                {" "}
+                Popularity
+              </Dropdown.Item>
 
-              <Dropdown.Item href="#/action-1" value="time" id="BreakfastId2" onClick={() => this.functionl(document.getElementById('BreakfastId2').getAttribute('value'))}>Time</Dropdown.Item>
+              <Dropdown.Item
+                href="#/action-1"
+                value="time"
+                id="BreakfastId2"
+                onClick={() =>
+                  this.functionl(
+                    document
+                      .getElementById("BreakfastId2")
+                      .getAttribute("value")
+                  )
+                }
+              >
+                Time
+              </Dropdown.Item>
 
-              <Dropdown.Item href="#/action-1" value="calories" id="BreakfastId3" onClick={() => this.functionl(document.getElementById('BreakfastId3').getAttribute('value'))}>Calories</Dropdown.Item>
+              <Dropdown.Item
+                href="#/action-1"
+                value="calories"
+                id="BreakfastId3"
+                onClick={() =>
+                  this.functionl(
+                    document
+                      .getElementById("BreakfastId3")
+                      .getAttribute("value")
+                  )
+                }
+              >
+                Calories
+              </Dropdown.Item>
             </DropdownButton>
 
             {this.state.showSort &&
-              this.state.sortedData
-                .map((element) => {
-                  console.log(element);
-                  return (
-                    <CategoryCreate
-                      showBySort={true}
-                      title={element.title}
-                      image={element.image}
-                      summary={element.summary}
-                      object={element.likes}
-                    />
-                  )
-                })}
-
-
-
+              this.state.sortedData.map((element) => {
+                console.log(element);
+                return (
+                  <CategoryCreate
+                    showBySort={true}
+                    title={element.title}
+                    image={element.image}
+                    summary={element.summary}
+                    object={element.likes}
+                  />
+                );
+              })}
           </Row>
         </div>
         <Row id="cardCategory">
-
           {this.state.show &&
             this.state.recipysData.map((element) => {
               return (
@@ -150,10 +192,8 @@ export class category extends Component {
                   summary={element.summary}
                   object={element.likes}
                 />
-
               );
-            })
-          }
+            })}
         </Row>
       </div>
     );
